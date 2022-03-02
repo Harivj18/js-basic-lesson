8e2fa167aa9669766124e1883686688e1de03017
Hariharan A <hari3027@global.chainsys.com>
2022-03-02 21:01:52 +0530

    first commit

---
+++ src/sample3.js
@@ -0,0 +1,11 @@
+/* eslint-disable semi */
+//  /*eslint no-use-before-define: "error" */
+function addNumbers (a, b) {
+  return a + b;
+}
+console.log(`2 + 3 => ${addNumbers(2, 3)}`);
+// console.log(` ${Multiplynumbers}`);
+function multiplyNumbers (a, b) {
+  return a * b;
+}
+console.log(`2 * 3 => ${multiplyNumbers(2, 3)}`);
