633b757b959ce42985c6ad50506f36dbcbd7e1a9
Hariharan A <hari3027@global.chainsys.com>
2022-03-08 11:13:49 +0530

    challenges

--- base-apparel-coming-soon-master/base.js
+++ base-apparel-coming-soon-master/base.js
@@ -1,38 +1,34 @@
-const inputEl = document.querySelector('#email')
+/* eslint-disable eqeqeq */
+const input = document.querySelector('#email')
 const btn = document.querySelector('#submit')
 const RegEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
-const iconErrorEl = document.querySelector('.icon-error')
-const textErrorEl = document.querySelector('.text-error')
-const fromGroupEl = document.querySelector('.form-group')
+const iconError = document.querySelector('.icon-error')
+const textError = document.querySelector('.text-error')
 let errors = []
 
 btn.addEventListener('click', e => {
-  // remove success class
-  textErrorEl.classList.remove('text-success')
+  textError.classList.remove('text-success')
 
-  // reset errors
   errors = []
-  // prevent default action of form
   e.preventDefault()
-  // validate email address
-  const email = inputEl.value
+  const email = input.value
 
   if (email == '' || email === undefined) {
     errors.push('Please provide us your email')
     // show error
-    iconErrorEl.style.display = 'block'
-    textErrorEl.innerText = errors[0]
+    iconError.style.display = 'block'
+    textError.innerText = errors[0]
   } else if (!email.match(RegEmail)) {
     errors.push('Please provide us your valid email')
     // show error icon
-    iconErrorEl.style.display = 'block'
-    textErrorEl.innerText = errors[0]
+    iconError.style.display = 'block'
+    textError.innerText = errors[0]
   }
 
   if (!errors.length > 0) {
     // hide error icon
-    iconErrorEl.style.display = 'none'
-    textErrorEl.classList.add('text-success')
-    textErrorEl.innerText = 'Thank you for subscribing to our newsletter.'
+    iconError.style.display = 'none'
+    textError.classList.add('text-success')
+    textError.innerText = 'Thanks for subscribing.We\'ll keep in touch with mail.'
   }
 })
