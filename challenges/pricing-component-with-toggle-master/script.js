901acdd64cc83f8188078ce87890a8083dc63938
Hariharan A <hari3027@global.chainsys.com>
2022-03-11 13:15:25 +0530

    challenges

---
+++ challenges/pricing-component-with-toggle-master/script.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+/* eslint-disable semi */
+document.querySelector('.toggle-switch__toggle')
+  .addEventListener('mouseup', function () {
+    const amounts = document
+      .getElementsByClassName('card__amount');
+    for (let i = 0; i < amounts.length; i++) {
+      const element = amounts[i];
+      const classList = element.classList;
+      classList.toggle('card__amount--hidden');
+    }
+  });
