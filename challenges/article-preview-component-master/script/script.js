194378a815c9b415d210d7b1d29447a92e6f99f4
Hariharan A <hari3027@global.chainsys.com>
2022-03-15 20:02:25 +0530

    challenges

---
+++ challenges/article-preview-component-master/script/script.js
@@ -0,0 +1,8 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable no-var */
+function myFunction () {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle('show');
+}
--- challenges/faq-accordion-card-main/script/script.js
+++ challenges/faq-accordion-card-main/script/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // Check screen and change images source
 checkScreen() // First load
 
